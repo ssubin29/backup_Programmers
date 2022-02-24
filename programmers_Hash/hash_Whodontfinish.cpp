75ce9e04b1f986b33df077012f4376f8017b20f7
ssubin29 <20201003@sungshin.ac.kr>
2022-02-24 22:12:43 +0900

    Update hash_Whodontfinish.cpp

--- programmers_Hash/hash_Whodontfinish.cpp
+++ programmers_Hash/hash_Whodontfinish.cpp
@@ -15,8 +15,7 @@ string solution(vector<string> participant, vector<string> completion) {
     for (auto elem : completion) //auto 키워드를 사용하여 컴파일러가 자료형을 추론하도록
     {
         if (strMap.end() == strMap.find(elem))
-            //end()는 빈 iterator의 주소 반환 
-            //find(a)의 경우 a를 찾지 못했으면 빈 iterator 주소 반환
+            //end()는 빈 iterator의 주소 반환 find(a)의 경우 a를 찾지 못했으면 빈 iterator 주소 반환
             //그러므로 end()와 find(a)의 값이 같을 경우 = a의 값을 찾지 못했을 경우
             strMap.insert(make_pair(elem, 1));
         //insert(make_pair(key,value)) : pair 형태로 추가 (반드시)!!
