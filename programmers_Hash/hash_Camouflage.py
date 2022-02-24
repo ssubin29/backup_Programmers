66d8739fd090b3b5fa6a0feaac64746537098f2c
ssubin29 <20201003@sungshin.ac.kr>
2022-02-25 00:26:22 +0900

    Create hash_Camouflage.py

---
+++ programmers_Hash/hash_Camouflage.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    answer = 1
+    clothes_count = {}
+    for cloth in clothes:
+        if (cloth[1] not in clothes_count.keys()):
+            clothes_count[cloth[1]] = 1
+        else :
+            clothes_count[cloth[1]] = clothes_count[cloth[1]] + 1
+            
+    for n in clothes_count.values():
+        answer = answer * (n+1)
+        
+    return answer-1
