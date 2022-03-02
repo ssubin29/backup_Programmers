0667e75d02a63645ff13ffbfa517d29c95dded9b
ssubin29 <20201003@sungshin.ac.kr>
2022-03-02 14:21:56 +0900

    Update staqu_Printer.py

--- programmers_StaQu/staqu_Printer.py
+++ programmers_StaQu/staqu_Printer.py
@@ -1,1 +1,10 @@
 # 스택/큐 - 프린터
+
+def solution(priorities, location):
+    answer = 0
+    
+    priorities_copy = priorities.copy()
+    for i in range(len(priorities)):
+        priorities_copy
+    
+    return answer
