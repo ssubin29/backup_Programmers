b639dcfe4f635cc500cc0842eec854f183ff4ec7
ssubin29 <20201003@sungshin.ac.kr>
2022-03-02 16:36:35 +0900

    Update staqu_StockPrice.py

--- programmers_StaQu/staqu_StockPrice.py
+++ programmers_StaQu/staqu_StockPrice.py
@@ -1,1 +1,16 @@
 # 스택/큐 - 주식가격
+
+def solution(prices):
+    answer = []
+    
+    for i in range(len(prices)-1):
+        count=1
+        for j in range(i+1,len(prices)-1):
+            if(prices[i]<=prices[j]):
+                count=count+1
+            else:
+                break
+        answer.append(count)
+    answer.append(0)
+    
+    return answer
