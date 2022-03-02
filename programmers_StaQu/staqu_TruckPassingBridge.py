409dc6b43ffc6b8c5ef9c23e76bdb52f3527280b
ssubin29 <20201003@sungshin.ac.kr>
2022-03-03 00:08:34 +0900

    Update staqu_TruckPassingBridge.py

--- programmers_StaQu/staqu_TruckPassingBridge.py
+++ programmers_StaQu/staqu_TruckPassingBridge.py
@@ -1,3 +1,13 @@
-/*
-스택/큐 - 다리를 지나는 트럭
-*/
+# 스택/큐 - 다리를 지나는 트럭
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    
+    bridge = []
+    bridge_weights = 0
+    
+    for i in truck_weight:
+        while(len(bridge)):
+            
+    
+    return answer
