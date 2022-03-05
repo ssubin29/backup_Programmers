94bbd7fa7a451106ade76614423e3b300bf1be67
ssubin29 <20201003@sungshin.ac.kr>
2022-03-05 21:30:21 +0900

    update final staqu_Development

--- programmers_StaQu/staqu_Development_last.py
+++ programmers_StaQu/staqu_Development_last.py
@@ -14,12 +14,15 @@ def solution(progresses, speeds):
     print("days는", days)
     # days는 progresses가 걸리는 일 수 
 
-    answer.append(1)    
-    # 차례대로 비교하며 num1 >= num2일 경우 그 값을 시발.....
-    for num1, num2 in zip(days, days[1:]):     
-        if num1 >= num2 :
+    answer.append(1)  
+    
+    # 차례대로 비교하며 num1 >= num2일 경우 그 값을 시발....
+    for i, (num1, num2) in enumerate(zip(days, days[1:])):
+        max_value = max(days[:i+1])
+        if num1 >= num2 or max_value >= num2 :
             answer[-1] = answer[-1] + 1
         else:
             answer.append(1)
-            
+    
+    #뭐가 잘못되었는지 알겠다.....
     return answer
