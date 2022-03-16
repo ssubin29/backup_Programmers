ed8acfd391bf7319f83a4caaba940297abf6765a
ssubin29 <20201003@sungshin.ac.kr>
2022-03-16 13:41:39 +0900

    Update sort_Knumber.py

--- programmers_Sort/sort_Knumber.py
+++ programmers_Sort/sort_Knumber.py
@@ -2,9 +2,15 @@
 
 def solution(array, commands):
     answer = []
+    
+    a=[2,5,1,6,3,4]
+    print(a[3:4])
+    
     for command in commands:
         if (command[0]==command[1]):
-            answer.append(array[command[0]])
+            #answer.append(array[command[0]]
+            print(1)
         else : 
-            answer.append(array[command[0]-1:command[1]-1][2])
+            #answer.append(array[command[0]-1:command[1]][command[2]])
+            print(0)
     return answer
