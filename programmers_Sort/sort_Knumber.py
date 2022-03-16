65bfdac21ea0e6f0def94f03145e440f0b46a828
ssubin29 <20201003@sungshin.ac.kr>
2022-03-16 13:10:26 +0900

    Update sort_Knumber.py

--- programmers_Sort/sort_Knumber.py
+++ programmers_Sort/sort_Knumber.py
@@ -1,1 +1,10 @@
 # K번째 수
+
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        if (command[0]==command[1]):
+            answer.append(array[command[0]])
+        else : 
+            answer.append(array[command[0]-1:command[1]-1][2])
+    return answer
