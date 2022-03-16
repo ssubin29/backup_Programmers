5bd38480a166eee067706035e494ace685b2406d
ssubin29 <20201003@sungshin.ac.kr>
2022-03-16 13:02:01 +0900

    sort

--- programmers_Sort/sort_BiggestN.py
+++ programmers_Sort/sort_BiggestN.py
@@ -1,1 +1,10 @@
 # 가장 큰 수
+
+def solution(numbers):
+    answer = ''
+    
+    strings = list(map(str, numbers))
+    strings.sort(reverse = True)
+    
+    print(strings)
+    return answer
--- programmers_Sort/sort_HIndex.py
+++ programmers_Sort/sort_HIndex.py
@@ -1,1 +1,15 @@
 # H-index
+
+def solution(citations):
+    answer = 0
+    
+    citations.sort()
+    print(citations)
+    
+    for i in range(1,len(citations)-1):
+        if i > citations[i-1]:
+            answer = i+1
+        else :
+            break
+            
+    return answer
