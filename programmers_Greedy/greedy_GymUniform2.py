4ed22c1030ad897dae08099d45472af0d8e09986
ssubin29 <20201003@sungshin.ac.kr>
2022-03-26 18:24:05 +0900

    Update greedy_GymUniform2.py

--- programmers_Greedy/greedy_GymUniform2.py
+++ programmers_Greedy/greedy_GymUniform2.py
@@ -1,5 +1,37 @@
 # 탐욕법 - 체육복
 
 def solution(n, lost, reserve):
-    answer = 0
+    answer = 0    
+    
+    student = [1 for i in range(n)]
+    for i in range(n):  
+        if i+1 in lost:
+            student[i] = student[i] - 1
+        if i+1 in reserve:
+            student[i] = student[i] + 1
+    #print('처음 : ', student)
+    
+    for i in range(n):
+        if i == 0:
+            if(student[i] == 2) and (student[i+1]==0):
+                student[i+1] = 1
+                student[i] = 1
+        elif i == n-1:
+             if(student[i] == 2) and (student[i-1]==0):
+                student[i-1] = 1
+                student[i] = 1
+        else:
+            if(student[i] == 2) and (student[i-1]==0):
+                student[i-1] = 1
+                student[i] = 1
+            elif(student[i] == 2) and (student[i+1]==0):
+                student[i+1] = 1
+                student[i] = 1
+    
+    #print('나눠준 후 : ', student)
+            
+    for stu in student:
+        if stu > 0 :
+            answer = answer + 1
+    
     return answer
