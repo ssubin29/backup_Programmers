e700de60834a366cf065fa8431ae99c49a2e2c1e
ssubin29 <20201003@sungshin.ac.kr>
2022-04-01 22:12:17 +0900

    Update greedy_Lifeboat.py

--- programmers_Greedy/greedy_Lifeboat.py
+++ programmers_Greedy/greedy_Lifeboat.py
@@ -1,6 +1,6 @@
 # 탐욕법 - 구명보트
 
-def solution(people, limit):
+def solution(people, limit): # 효율성 0점
     answer = 0
     peoples_r =  people.copy()
     peoples =  people.copy()
@@ -8,17 +8,21 @@ def solution(people, limit):
     peoples_r.sort(reverse=True)
     peoples.sort()
     
-    print(peoples_r)
-    print(peoples)
-    print(list(enumerate(people)))
     
     weight = 0
     while(people):
         weight = peoples_r[0]
+        answer = answer + 1
         people.remove(weight)
         peoples.remove(weight)
         peoples_r.remove(weight)
-        while(weight <= limit):
-            #for people
+        for p in peoples:
+            if(weight + p > limit):
+                break
+            else:
+                weight = weight + peoples[0]
+                people.remove(peoples[0])
+                peoples_r.remove(peoples[0])
+                peoples.remove(peoples[0])
         
     return answer
