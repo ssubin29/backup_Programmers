4494033d125b5ba3290b636602f23553d390e7f4
ssubin29 <20201003@sungshin.ac.kr>
2022-04-02 22:58:20 +0900

    Update greedy_Joystick.py

--- programmers_Greedy/greedy_Joystick.py
+++ programmers_Greedy/greedy_Joystick.py
@@ -1,58 +1,89 @@
 # 탐욕법 - 조이스틱
 
 import itertools
-
+import copy
+    
 # 최단 경로를 찾아주는 함수
 # 입력값 : 시작 인덱스, 목표 인덱스, 총 길이
 def get_better_way(start, des, limit_len): 
     better = 0
     a = abs(des-start) # A~Z 사이를 왔다갔다 ex)'A'>'C'는 이게 좋음
-    b = start + (limit_len - des) # Z를 넘아 A부터 ex) 'Z'>'C'는 이게 좋음
-    return a if a < b else b
+    b = start + (limit_len - des) + 1 # Z를 넘아 A부터 ex) 'Z'>'C'는 이게 좋음
+    return a if a < b else b # 작은 거리 반환
 
 def solution(name):
     answer = 0
-    theN = 65 # 시작은 A
     # A는 65 Z는 90
     
-    # 인덱싱 리스트 만들기 Why? 이제부터 알파벳 완성 순서를 뒤바꿀거라 위치를 기억해두어야 함
-    # 예를 들어, ZAF를 AFZ 순서로 완성하기 위해선 그 문자가 위치하는 인덱스를 알아내 
-    # 그만큼 이동하는 조작까지 추가해야한다.
-    indexing_list = {}
+    # 알파벳과 알파벳이 위치해있는 인덱스 매핑
+    A_index_list
     for i in range(len(name)):
         if not name[i] in indexing_list.keys():
             indexing_list[name[i]] = [i]
         else:
             indexing_list[name[i]].append(i)
     print(indexing_list)
-    
-    # 이제부터 그리디 알고리즘의 진면목.. 조합을 사용하여 문자 완성 순서 뒤바꾸기
-    orders = list(set((list(map(''.join, itertools.permutations(name))))))
-    print(orders)
-    
-    # 그리고 각 문자 완성 순서대로 돌면서 최솟값 찾기
+
     # 중요한 것은 여기서 세는 것은 오직 조이스틱을 위아래로 조작한 횟수 뿐이다.
     alpha_len = ord('Z')-ord('A')
-    print(alpha_len)
-    answer_list = []
-    for order in orders:
-        count = 0
-        start = get_better_way(0, ord(order[0])-ord('A'), alpha_len)
-        # start는 첫번째 조작을 시작할 위치
-        for alpha in order[1:]:            
-            count = count + get_better_way(start, ord(alpha)-ord('A'), alpha_len)
-        answer_list.append(count)
-    print(answer_list)
+    updown_count = 0
+    for alpha in name:
+        updown_count = updown_count + get_better_way(0, ord(alpha)-ord('A'), alpha_len)    
+    answer += updown_count  
+    
+    # 지금부터는 상하로 조작하는 횟수만 카운트한다.
+    # 여기서 주의! 문자열에 A가 없다면 문자열의길이-1이 상하조작 횟수지만
+    # 붙어있는 A가 있다면(두 세트 이상부터는 상관x) 그 부분은 조작할 필요가 없으니까 그 부분을 뺴고 왼쪽으로 돌게 된다
+    Acount = 0
+    opened = False # CAAT 처럼 A가 한 세트 연달아 있을 때
+    closed_by_A = False # 말그대로 A 세트가 앞뒤에만 있을 때 AADFA cf)ABSTRACTA
+    if 'A' in indexing_list.keys():
+        A_list = indexing_list['A']       
 
-    # 지금부터 세는 건 조이스틱을 왼쪽 오른쪽으로 이동시키는 횟수
-    for i in range(len(orders)):
-        order_list = []
-        for alpha in orders[i]:
-            order_list.append(indexing_list[alpha])
-        count = 0
-        start = get_better_way(0, order_list[0], len(name))
-        for order in order_list[1:]:
-            count = count + get_better_way(0, order, len(name))
-    return answer
+        # opened부터 판별
+        if len(A_list) == 1:
+            Acount += 1
+            opened = True
+        else:
+            a = [i for i in range(len(name))]
+            s = A_list[0]
+            e = A_list[-1]
+            print(A_list)
+            if A_list == a[A_list[0]:A_list[-1]+1]:
+                Acount = len(A_list)
+                opened = True
+        if (opened):
+            print('opened입니다')
+            return answer + (len(name) - 1 ) - Acount
+        print('opened는 아닙니다')
 
-print(solution('ZAZ'))
+        # closed_by_A 판별
+        A_list = indexing_list['A']
+        if (0 in A_list) and (len(name)-1 in A_list):
+            c = 0
+            for a_index in A_list[1:]:
+                if c + 1 == a_index:
+                    A_list.remove(c+1)
+                    Acount += 1
+                    c = a_index
+                else:
+                    break
+            for a_index in A_list[::-1]:
+                if c - 1 == a_index:
+                    A_list.remove(c-1)
+                    Acount += 1
+                    c = a_index
+                else:
+                    break
+            if not (A_list):
+                closed_by_A = True
+        if (closed_by_A):
+            print('closed_by_A입니다')
+            return answer + (len(name) - 1 ) - Acount        
+        print('closed_by_A 또한 아닙니다')
+
+    
+    return answer + (len(name) - 1 )
+
+
+print(solution("BBAAAACC"))
