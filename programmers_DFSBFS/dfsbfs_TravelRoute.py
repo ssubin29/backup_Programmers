55bf7b98f531d65ec77087a884f5537924cd16a2
ssubin29 <20201003@sungshin.ac.kr>
2022-04-09 20:36:10 +0900

    Update dfsbfs_TravelRoute.py

--- programmers_DFSBFS/dfsbfs_TravelRoute.py
+++ programmers_DFSBFS/dfsbfs_TravelRoute.py
@@ -1,23 +1,26 @@
 # 깊이/너비 우선 탐색(DFS/BFS) - 여행경로
 
 from collections import deque
+import sys
+sys.setrecursionlimit(10**7)
 
 # BFS 함수 정의
-def bfs(dict_of_ticket, start, length):
+def bfs(dict_of_ticket, start, i, length, ans):
     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
     queue = deque([start])
-    # 현재 노드를 방문 처리
-    visited[i] = True
-    # 큐가 빌 때까지 반복
-    while queue:
-        # 큐에서 하나의 원소를 뽑아 출력
-        v = queue.popleft()
-        print(v, end=' ')
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+    ans.append(start)
+    if start in dict_of_ticket.keys():
+        if length == len(ans):
+            print(ans)
+        else:
+            # 큐가 빌 때까지 반복
+            while queue:
+                # 큐에서 하나의 원소를 뽑아 출력
+                start = queue.popleft()        
+                for i in range(len(dict_of_ticket[start])):
+                    bfs(dict_of_ticket, dict_of_ticket[start][i], i, length, ans)
+            
+        
 
 def solution(tickets):
     answer = []
@@ -32,7 +35,7 @@ def solution(tickets):
     
     length = len(dict_t.keys())    
     #visited = [False] * length
-    dfs(dict_t, "ICN", length)
+    answer = bfs(dict_t, "ICN", 0, length, answer)
         
     
     return answer
