b504b3d21988e0906252baeffd0d978bff416110
ssubin29 <20201003@sungshin.ac.kr>
2022-04-17 16:25:01 +0900

    Update greedy_Joystick.py

--- programmers_Greedy/greedy_Joystick.py
+++ programmers_Greedy/greedy_Joystick.py
@@ -81,7 +81,6 @@ def solution(name):
             #print('closed_by_A입니다')
             return answer + (len(name) - 1 ) - Acount        
         #print('closed_by_A 또한 아닙니다')
-        
     return answer + (len(name) - 1 )
 
 print(solution("BBAAAACC"))
